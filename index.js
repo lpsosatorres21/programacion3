41db7a621e79094bf7d09cdeb6f4c5022198dbd7
unknown <lpsosatorres21@gmail.com>
2020-04-22 17:51:28 +0200

    Api version 1.0.0

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const app = express();
+const port = process.env.PORT || 3003
+
+const apiVersion = {
+    name : 'Sample Express Api',
+    version: '1.0.0',
+    author: 'nigga',
+    contactInfo: {
+        facebook: 'Nigga Sosa',
+        Insta: 'Lautaro Sosa', 
+        twitter: 'Wcio'
+        
+    }
+}
+
+app.get('/', function(req, res){
+    res.send('Hello word')
+})
+
+app.get('/version' , (req,res) =>{
+    res.send(apiVersion.version)
+})
+
+app.get('/version/cantacto' , (req,res) =>{
+    res.send(apiVersion.contactInfo)
+})
+
+
+
+app.listen(port,()=>console.log('Escuchando en el puerto ' + port))
